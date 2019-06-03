6107811f806876f0f46c5c86c938412afb8abfbb
miyamoen <akihito.biblioteca@gmail.com>
2019-06-03 09:47:40 +0900

    Completed!

--- Main.elm
+++ Main.elm
@@ -3,7 +3,16 @@ module Main exposing (main)
 import Browser exposing (Document, UrlRequest(..))
 import Browser.Navigation as Nav exposing (Key)
 import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events exposing (onClick)
+import Element.Font as Font
+import Element.Input exposing (button)
 import Html
+import Html.Events
+import Json.Decode as JD
+import List.Extra
+import Time
 import Url exposing (Url)
 import Url.Builder as UB
 import Url.Parser as UP exposing ((</>), s)
@@ -15,7 +24,7 @@ main =
         { init = init
         , view = view
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = always <| Time.every 32 Tick
         , onUrlRequest = ClickedLink
         , onUrlChange = UrlChanged
         }
@@ -25,6 +34,7 @@ type alias Model =
     { route : Route
     , key : Key
     , items : List Item
+    , angle : Float
     }
 
 
@@ -32,6 +42,8 @@ type Msg
     = NoOp
     | ClickedLink UrlRequest
     | UrlChanged Url
+    | Tick Time.Posix
+    | ToggleRotateItem String
 
 
 type Route
@@ -40,7 +52,12 @@ type Route
 
 
 type alias Item =
-    { id : String }
+    { id : String
+    , name : String
+    , description : String
+    , isRotation : Bool
+    , angle : Float
+    }
 
 
 routeparser : Url -> Route
@@ -70,7 +87,12 @@ init : () -> Url -> Key -> ( Model, Cmd Msg )
 init _ url key =
     ( { route = routeparser url
       , key = key
-      , items = [ Item "Ham", Item "Spam", Item "Egg" ]
+      , items =
+            [ Item "ham" "ハム" "ハムはおいしいよ" False 0
+            , Item "spam" "スパム" "スパムは教養" False 0
+            , Item "egg" "たまご" "だし巻き" False 0
+            ]
+      , angle = 0
       }
     , Cmd.none
     )
@@ -97,6 +119,34 @@ update msg model =
             , Cmd.none
             )
 
+        Tick posix ->
+            ( { model
+                | angle =
+                    Time.posixToMillis posix
+                        // 2
+                        |> modBy 360
+                        |> toFloat
+                        |> degrees
+              }
+            , Cmd.none
+            )
+
+        ToggleRotateItem id ->
+            ( { model
+                | items =
+                    List.Extra.updateIf (.id >> (==) id)
+                        (\item ->
+                            if item.isRotation then
+                                { item | isRotation = False, angle = model.angle - item.angle }
+
+                            else
+                                { item | isRotation = True, angle = -model.angle }
+                        )
+                        model.items
+              }
+            , Cmd.none
+            )
+
 
 view : Model -> Document Msg
 view model =
@@ -105,23 +155,120 @@ view model =
     }
 
 
-router : Model -> Element msg
+router : Model -> Element Msg
 router model =
     case model.route of
         TopRoute ->
-            column []
-                [ text "top"
-                , link []
-                    { url = itemUrl "Ham"
-                    , label = text <| "item link Ham"
-                    }
-                ]
+            topPage model
 
         ItemRoute id ->
-            column []
-                [ text <| "item : " ++ id
-                , link []
-                    { url = topUrl
-                    , label = text <| "return to Top"
+            itemPage id model
+
+
+topPage : Model -> Element Msg
+topPage { items, angle } =
+    column [ padding 48, spacing 32, width fill ]
+        [ text "Elmでaタグの遷移を止めて別のことをするサンプル"
+        , wrappedRow [ spacing 16, width fill ] <|
+            List.map (itemCard angle) items
+        ]
+
+
+itemCard : Float -> Item -> Element Msg
+itemCard gloabalAngle { id, name, description, isRotation, angle } =
+    link
+        [ width <| px 300
+        , padding 24
+        , Border.width 2
+        , height fill
+        ]
+        { url = itemUrl id
+        , label =
+            column [ spacing 24, height fill ]
+                [ el [ Font.size 24 ] <| text name
+                , image
+                    [ width fill
+                    , if isRotation then
+                        rotate <| gloabalAngle - angle
+
+                      else
+                        rotate angle
+                    ]
+                    { src = "/img/" ++ id ++ ".png"
+                    , description = name ++ "の画像"
                     }
+                , paragraph [] [ text description ]
+                , el
+                    [ Border.rounded 10
+                    , paddingXY 16 8
+                    , Background.color <| rgb255 255 185 116
+                    , preventUrlRequest <| ToggleRotateItem id
+                    , pointer
+                    , alignBottom
+                    , alignRight
+                    ]
+                  <|
+                    if isRotation then
+                        text "止める"
+
+                    else
+                        text "回す"
                 ]
+        }
+
+
+itemPage : String -> Model -> Element Msg
+itemPage id model =
+    column [ padding 48, spacing 32, width fill ]
+        [ link []
+            { url = topUrl
+            , label = text <| "戻る"
+            }
+        , case List.Extra.find (.id >> (==) id) model.items of
+            Just { name, description, isRotation, angle } ->
+                column [ spacing 24 ]
+                    [ el [ Font.size 24 ] <| text name
+                    , image
+                        [ width fill
+                        , if isRotation then
+                            rotate <| model.angle - angle
+
+                          else
+                            rotate angle
+                        ]
+                        { src = "/img/" ++ id ++ ".png"
+                        , description = name ++ "の画像"
+                        }
+                    , paragraph [] [ text description ]
+                    , el
+                        [ Border.rounded 10
+                        , paddingXY 16 8
+                        , Background.color <| rgb255 255 185 116
+                        , onClick <| ToggleRotateItem id
+                        , pointer
+                        , alignBottom
+                        , alignRight
+                        ]
+                      <|
+                        if isRotation then
+                            text "止める"
+
+                        else
+                            text "回す"
+                    ]
+
+            Nothing ->
+                text <| "id : " ++ id ++ " は見つかりませんでした"
+        ]
+
+
+preventUrlRequest : msg -> Attribute msg
+preventUrlRequest msg =
+    Html.Events.custom "click"
+        (JD.succeed
+            { message = msg
+            , stopPropagation = True
+            , preventDefault = True
+            }
+        )
+        |> htmlAttribute
